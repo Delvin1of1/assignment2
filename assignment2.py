75d41641f39d8f28e4e8ae0ae1b55474e77f1434
Delvin1of1 <159158944+Delvin1of1@users.noreply.github.com>
2025-01-25 18:12:18 -0500

    added assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,21 @@
+username = input("what is your name: ").title()
+display = len(username)
+display2 = len(username.split())
+first_word = username.split()[0]
+last_word = username.split()[-1]
+first_three = username[:3]
+last_three = username[-3:]
+display_reverse = username[::-1]
+display_reverse = "".join(reversed(username))
+replace = username.replace(" ", "-")
+print(username)
+print(display)
+print(display2)
+print(first_word)
+print(last_word)
+print(first_three)
+print(last_three)
+print(display_reverse)
+print(username.upper())
+print(username.lower())
+print(replace)
